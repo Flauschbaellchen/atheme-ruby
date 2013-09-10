db4ed97646c6e95ae7f47040cf01a11abb726b96
Noxx <noxx@penya.de>
2013-09-10 17:30:07 +0200

    Let chanserv#list and hostserv#listvhost respond with a Atheme::LIst object

--- lib/atheme/services/chanserv.rb
+++ lib/atheme/services/chanserv.rb
@@ -11,13 +11,18 @@ module Atheme
         output.delete_at(0)
         output.delete_at(output.length - 1)
 
-        output.map do |info|
+        output.map! do |info|
           out = info.sub('- ', '').split('(')
           channel = out[0].sub(' ', '')
           owner = out[1].sub(')', '').sub(' ', '')
 
-          { channel: channel, owner: owner }
+          {
+            channel: Atheme::Channel.new(session, channel),
+            founder: Atheme::User.new(session, owner)
+          }
         end
+
+        Atheme::Helpers::List.new(output)
       end
     end
   end
--- lib/atheme/services/hostserv.rb
+++ lib/atheme/services/hostserv.rb
@@ -1,4 +1,17 @@
 module Atheme
   class HostServ < Service
+
+    parse :listvhost do
+      command :to_a do
+        output = raw_output.split("\n")[0..-2]
+
+        output.map! do |info|
+          out = info.split(" ")
+          { user: out[1], vhost: out[2] }
+        end
+
+        Atheme::Helpers::List.new(output)
+      end
+    end
   end
 end
